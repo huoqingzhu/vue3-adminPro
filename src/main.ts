e859c051d64616ac1c8d64493779daa2698be7f0
霍庆祝 <1533132103@qq.com>
2021-03-16 18:38:15 +0800

    修改md

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import store from "./store";
-
 import ant from "./utils/ant";
 import "@/utils/permission"//配置路由导航守卫
 import "@/style/index.less"
